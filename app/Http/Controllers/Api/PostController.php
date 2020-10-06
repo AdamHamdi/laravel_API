10fe4fd5d465e22dcf89e644f37a11f80f2cbb7f
Adam Hamdi <prodev.webdesign@gmail.com>
2020-10-06 22:17:59 +0100

    first commit 06/10/2020

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Post;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // 1-all are not allow
+        //2-meta data not load
+        //3 DB-schema exposed
+        // $posts=Post::all();
+        // return response()->json([
+        //     'data'=>$posts,
+        //      'request-date'=>date('Y-M-D'),
+
+        // ],200) ;
+        return Post::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/TopicalityController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Topicality;
+use Illuminate\Http\Request;
+
+class TopicalityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $topicalities=Topicality::orderByDesc('created_at')->get();
+        return $topicalities->toJson(JSON_PRETTY_PRINT);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $topicalities=Topicality::create($request->all());
+        if($topicalities){
+            return response()->json([
+                'Success'=>'La topicalitée a été ajoutée avec succès'
+            ],200);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Topicality  $topicality
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Topicality $topicality)
+    {
+        return $topicality;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Topicality  $topicality
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Topicality $topicality)
+    {
+        //
+        $topicality->update($request->all());
+        if($topicality){
+        return response()->json([
+            'Success'=>'La topicalitée a été modifiée avec succès'
+        ],200);
+    }
+}
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Topicality  $topicality
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Topicality $topicality)
+    {
+        //
+        $topicality->delete();
+    }
+}
